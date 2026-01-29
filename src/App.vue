7a29c1dc12f8f6e0b0fe5fdd69276fff1b8fd80e
mserawsi <mserawsi@gmail.com>
2026-01-29 15:20:48 -0800

    [INTR-1] Intial Commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import ExampleImg from './components/ExampleImg.vue'
+import HeroSection from './components/HeroSection.vue'
+import NavGrid from './components/NavGrid.vue'
+</script>
+
+<template>
+  <div>
+    <ExampleImg />
+    <HeroSection
+      title="Lorem ipsum"
+      subtitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+      image="https://assets.pkimgs.com/pkimgs/ab/images/dp/ecm/202529/2559/001/002.jpg"
+      image-alt="Lorem ipsum Hero"
+    />
+    <NavGrid />
+  </div>
+</template>
+
+<style lang="scss"></style>
---
+++ src/components/HeroSection.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="abox1975995395" class="seo-common-header-hero mac-component-wrapper mac-abox-wrapper responsive-devicetype-desktop responsive-devicetype-mobile">
+    <div class="aboxDefault">
+      <div id="imgplaceholder1049447811" class="section imgplaceholder mac-component-wrapper mac-image-wrapper responsive-devicetype-desktop responsive-devicetype-not-mobile seo-common-header-hero-image">
+        <img class="" src="https://assets.pkimgs.com/pkimgs/ab/images/dp/ecm/202529/2559/001/002.jpg" alt="Lorem ipsum Hero">
+      </div>
+      <div id="imgplaceholder174020977" class="section imgplaceholder mac-component-wrapper mac-image-wrapper responsive-devicetype-not-desktop responsive-devicetype-mobile seo-common-header-hero-image">
+        <!---->
+      </div>
+      <div id="text-616612980" class="text section seo-common-header-hero-title mac-component-wrapper mac-text-wrapper responsive-devicetype-desktop responsive-devicetype-mobile">
+        <h1>Lorem ipsum</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  title: {
+    type: String,
+    default: 'Lorem ipsum'
+  },
+  subtitle: {
+    type: String,
+    default: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+  },
+  image: {
+    type: String,
+    default: 'https://assets.pkimgs.com/pkimgs/ab/images/dp/ecm/202529/2559/001/002.jpg'
+  },
+  imageAlt: {
+    type: String,
+    default: 'Hero image'
+  }
+})
+</script>
